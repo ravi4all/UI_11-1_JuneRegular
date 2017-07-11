30a0f5367d1699ad689ffc40b12e9cc5b113fb27
ravi4all <ravikanttyagi.2295@gmail.com>
2017-07-11 12:56:13 +0530

    Add files via upload

---
+++ 06-Javascript/02-JS_Syntax/external.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function(){
+    document.getElementById("show").addEventListener("click", function(){
+        a = document.getElementById("box_1").value;
+        document.getElementById("result").innerHTML = a;
+    })
+})
+
+
+/*
+function show(){
+    a = document.getElementById("box_1").value;
+    document.getElementById("result").innerHTML = a;
+}*/
